8901b367e61e02bd4b913b27994180cd93659def
Jaroslav Prokofiev <kostyi.pererostokvb@gmail.com>
2024-04-21 18:34:58 +0300

    create model in prisma

--- bookhaven-back-end/src/jaroslav/jaroslav.controller.ts
+++ bookhaven-back-end/src/jaroslav/jaroslav.controller.ts
@@ -4,4 +4,23 @@ import { JaroslavService } from './jaroslav.service';
 @Controller('jaroslav')
 export class JaroslavController {
   constructor(private readonly jaroslavService: JaroslavService) {}
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('register')
+  async register(@Body() dto: UserAuthDto){
+    return this.userauthService.register(dto); 
+   }
+   @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: UserAuthDto){
+    return this.userauthService.login(dto); 
+   }
+   @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login/access-token')
+  async NewTokens(@Body() dto: refreshTokenDto){
+    return this.userauthService.NewTokens(dto.refreshToken); 
+   }
 }
